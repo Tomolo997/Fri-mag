bed6798f011e9b300608dc308a30dd50ae42ae9c
Tomaž Ovsenjak <tomazovsenjak@Tomazs-Air.fritz.box>
2021-07-08 11:11:31 +0200

    finished chapter One

---
+++ programiranje/Chapter_One.py
@@ -0,0 +1,63 @@
+#Python is an interpreted, object-oriented, high-level
+# programming language with dynamic semantics
+#Algorithm is a fancy word for a detailed recipe of instructions
+
+#1) Numbers and expression
+print(1/2) # => returns 0 
+print(1.0/2.0) # => returns 0.5
+
+# Realna stevila so plavajoca, real => floats
+# // double stash vedno vzame cela stevila.
+
+# % => remainder, to kar ostane 
+# 10 % 3 = 1  => ostanek je rezultat
+# + => addition 
+# - => substraction 
+# / => division 
+# * => multiplicaiton 
+# ** => to the power of 
+
+#VELIKA STEVILA 
+# Navadna stevila ne smejo biti vecja od 2137483647 in manjsa od 
+# - 2147483648
+
+
+#Variables 
+# name that represents some value 
+x = 3
+y = 12
+print(x+y)
+
+#Statements and expression ,
+# 2*2 => statemens, they change things  , for example => (x = 12)
+#print 2*2 => expression, they dont change things
+
+input("Please enter your lucky number")
+
+#>>> x = input("x: ")
+#x: 34
+#>>> y = input("y: ")
+#y: 42
+#>>> print x * y
+#1428
+
+# functions 
+# little program that performs a specific action
+# function returns a value to you, it is another type of expression
+
+
+#Modules 
+# Extensions that can be imported into python, to extend its
+#capabilites 
+#import math => than use it while calling function on it
+#from math import sqrt => only imports sqrt => square root
+
+
+#__future__  __future__. From it,
+#we can import features that will be standard in Python in the future but that aren’t part of the
+#language yet. 
+
+#Strings
+#Strings are found in almost every useful, real-world Python program and have
+#many uses. Their main use is to represent bits of text, such as the exclamation “Hello, world!”
+#Strings are values, just as numbers are
